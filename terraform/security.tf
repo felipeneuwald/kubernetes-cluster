91ad7944ff536b9174d923d40f7da276508eb9f9
Felipe Neuwald <felipebgn@gmail.com>
2020-02-08 22:59:26 +0000

    Add kube-apiserver security group

--- terraform/security.tf
+++ terraform/security.tf
@@ -36,3 +36,42 @@ resource "aws_security_group" "kubernetes-cluster" {
     Project     = "kubernetes-cluster"
   }
 }
+
+resource "aws_security_group" "kube-apiserver" {
+  name        = "kube-apiserver"
+  description = "kube-apiserver"
+  vpc_id      = aws_vpc.kubernetes-cluster.id
+
+  ingress {
+    protocol    = -1
+    self        = true
+    from_port   = 0
+    to_port     = 0
+    description = "Self traffic"
+  }
+
+  egress {
+    from_port     = 0
+    to_port       = 0
+    protocol      = "-1"
+    cidr_blocks   = [
+      "0.0.0.0/0"
+    ]
+    description   = "Outbound traffic"
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    description = "kube-apiserver"
+  }
+
+  tags = {
+    Name        = "kube-apiserver"
+    Project     = "kubernetes-cluster"
+  }
+}
